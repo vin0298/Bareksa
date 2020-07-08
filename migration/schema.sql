23e578aa238e35ae29ced2a6aa10b802628139e5
Vincent Surya Bensawan <bensawan0298@gmail.com>
2020-07-08 08:06:38 -0700

    Created a SQL seeder file for testing

--- migration/schema.sql
+++ migration/schema.sql
@@ -11,13 +11,13 @@ CREATE TABLE news_articles (
     status VARCHAR(25) NOT NULL
 );
 
-CREATE TABLE tags (
+CREATE TABLE tags_tw (
     tag_id SERIAL PRIMARY KEY,
     tag_name VARCHAR(255) NOT NULL unique,
     uuid UUID NOT NULL
 );
 
-CREATE TABLE news_tags (
+CREATE TABLE news_tags_tw (
     article_id INT NOT NULL REFERENCES news_articles(article_id) ON UPDATE CASCADE ON DELETE CASCADE,
     tag_id INT NOT NULL REFERENCES tags(tag_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
