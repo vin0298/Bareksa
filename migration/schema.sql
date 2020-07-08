87b7fbfdf53fcd0b8a4fcda817e3865eb9296574
Vincent Surya Bensawan <bensawan0298@gmail.com>
2020-07-08 07:28:59 -0700

    Created a database schema

---
+++ migration/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS news_articles, news_tags, tags;
+
+CREATE TABLE news_articles (
+    article_id SERIAL PRIMARY KEY,
+    author VARCHAR(50) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    time_published TIMESTAMPTZ  NOT NULL,
+    uuid UUID NOT NULL,
+    topic VARCHAR(255) NOT NULL,
+    status VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE tags (
+    tag_id SERIAL PRIMARY KEY,
+    tag_name VARCHAR(255) NOT NULL unique,
+    uuid UUID NOT NULL
+);
+
+CREATE TABLE news_tags (
+    article_id INT NOT NULL REFERENCES news_articles(article_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    tag_id INT NOT NULL REFERENCES tags(tag_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
